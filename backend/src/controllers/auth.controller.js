f5358bda743a609f87b26dc76a73d96e03376b8a
Arego11 <aregar20012001@gmail.com>
2025-06-04 23:58:39 -0700

    init

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signup = () => {
+    res.send("signup route")
+}
+
+export const signin = () => { 
+    res.send("signin route")
+}
+
+export const signout = () => {
+    res.send("signout route")
+}
